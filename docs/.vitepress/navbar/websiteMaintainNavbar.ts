bf487877adcd9234d2d593be1e9640d4b23d8326
zeanzai <zeanzai.me@gmail.com>
2025-08-26 19:00:32 +0800

    修改

--- docs/.vitepress/navbar/websiteMaintainNavbar.ts
+++ docs/.vitepress/navbar/websiteMaintainNavbar.ts
@@ -2,5 +2,5 @@ import { DefaultTheme } from "vitepress";
 
 export const websiteMaintainNavbar: DefaultTheme.NavItem = {
   text: "网站维护",
-  link: "/网站维护/seata/seata的安装",
+  link: "/网站维护/ci的使用",
 };
