c3e4df4f5bf0e3d677c27c8102d0e8ef334ccb7c
zeanzai <zeanzai.me@gmail.com>
2025-08-28 17:54:35 +0800

    修改sidebar的读取方式

--- docs/.vitepress/sidebar/mianzhasidebar.ts
+++ docs/.vitepress/sidebar/mianzhasidebar.ts
@@ -1,50 +1,53 @@
-import { DefaultTheme } from "vitepress";
+import { DefaultTheme } from 'vitepress';
 
 export const mianzhasidebar: DefaultTheme.Sidebar = [
   {
-    text: "Java",
-    base: "/面渣",
+    text: 'Java',
+    base: '/面渣',
     items: [
       // { text: "java 基础", link: "/01-javase" },
       // { text: "java 集合", link: "/02-collection" },
       // { text: "java 线程", link: "/03-javathread" },
-      { text: "jvm", link: "/jvm" },
-      { text: "linux", link: "/linux" },
+      { text: 'jvm', link: '/jvm' },
+      { text: 'linux', link: '/linux' },
     ],
   },
   {
-    text: "框架",
-    base: "/面渣",
+    text: '框架',
+    base: '/面渣',
     items: [
-      { text: "微服务", link: "/weifuwu" },
-      { text: "spring", link: "/spring" },
-      { text: "protobuff", link: "/protobuff" },
-      { text: "Netty", link: "/netty" },
+      { text: '微服务', link: '/weifuwu' },
+      { text: 'spring', link: '/spring' },
+      { text: 'protobuff', link: '/protobuff' },
+      { text: 'Netty', link: '/netty' },
     ],
   },
   {
-    text: "中间件",
-    base: "/面渣",
+    text: '中间件',
+    base: '/面渣',
     items: [
-      { text: "MySQL", link: "/mysql" },
-      { text: "Mybatis", link: "/mybatis" },
-      { text: "redis", link: "/redis" },
-      { text: "rocketmq", link: "/rocketmq" },
+      { text: 'MySQL', link: '/mysql' },
+      { text: 'Mybatis', link: '/mybatis' },
+      { text: 'redis', link: '/redis' },
+      { text: 'rocketmq', link: '/rocketmq' },
     ],
   },
   {
-    text: "分布式",
-    base: "/面渣",
-    items: [{ text: "分布式", link: "/fenbushi" }],
+    text: '分布式',
+    base: '/面渣',
+    items: [{ text: '分布式', link: '/fenbushi' }],
   },
   {
-    text: "软件质量管理",
-    base: "/面渣",
+    text: '软件质量管理',
+    base: '/面渣',
     items: [],
   },
   {
-    text: "工程设计",
-    base: "/面渣",
-    items: [{ text: "nixi", link: "/nixi" }],
+    text: '工程设计',
+    base: '/面渣',
+    items: [{ text: 'nixi', link: '/nixi' }],
   },
 ];
+
+export default mianzhasidebar;
+export const path = '/面渣/';
---
+++ docs/.vitepress/sidebar_bak/sourcecodeSidebar.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "vitepress";
+
+export const sourcecodeSidebar: DefaultTheme.Sidebar = [
+    {
+        text: "seata",
+        base: "/项目实战/seata",
+        items: [
+            { text: "seata的安装", link: "/seata的安装" },
+            { text: "分布式事务TCC模式", link: "/分布式事务tcc模式" },
+        ],
+    },
+    {
+        text: "rabbitmq",
+        base: "/项目实战/rabbitmq",
+        items: [
+            { text: "安装rabbitmq", link: "/安装" },
+            { text: "基础", link: "/基础使用" },
+            { text: "高阶", link: "/高阶使用" },
+        ],
+    },
+];
