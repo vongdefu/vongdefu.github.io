7b5154f5e6981c7c298076f40f8ceebd4ee86db2
zeanzai <zeanzai.me@gmail.com>
2025-08-28 18:19:21 +0800

    Revert "修改sidebar的读取方式"
    This reverts commit c3e4df4f5bf0e3d677c27c8102d0e8ef334ccb7c.

--- docs/.vitepress/sidebar/sourcecodeSidebar.ts
+++ docs/.vitepress/sidebar/sourcecodeSidebar.ts
@@ -1,24 +1,21 @@
-import { DefaultTheme } from 'vitepress';
+import { DefaultTheme } from "vitepress";
 
 export const sourcecodeSidebar: DefaultTheme.Sidebar = [
-  {
-    text: 'seata',
-    base: '/项目实战/seata',
-    items: [
-      { text: 'seata的安装', link: '/seata的安装' },
-      { text: '分布式事务TCC模式', link: '/分布式事务tcc模式' },
-    ],
-  },
-  {
-    text: 'rabbitmq',
-    base: '/项目实战/rabbitmq',
-    items: [
-      { text: '安装rabbitmq', link: '/安装' },
-      { text: '基础', link: '/基础使用' },
-      { text: '高阶', link: '/高阶使用' },
-    ],
-  },
+    {
+        text: "seata",
+        base: "/项目实战/seata",
+        items: [
+            { text: "seata的安装", link: "/seata的安装" },
+            { text: "分布式事务TCC模式", link: "/分布式事务tcc模式" },
+        ],
+    },
+    {
+        text: "rabbitmq",
+        base: "/项目实战/rabbitmq",
+        items: [
+            { text: "安装rabbitmq", link: "/安装" },
+            { text: "基础", link: "/基础使用" },
+            { text: "高阶", link: "/高阶使用" },
+        ],
+    },
 ];
-
-export default sourcecodeSidebar;
-export const path = '/项目实战/';
