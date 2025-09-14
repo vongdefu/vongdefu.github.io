2c067ea12332cd5d5ee9783ee390d0a5fc9429ff
zeanzai <zeanzai.me@gmail.com>
2025-09-14 17:53:59 +0800

    添加被误删的网站配置相关的文件；

---
+++ docs/.vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="margin-top: 24px">
+    <Giscus
+      id="comments"
+      repo="vongdefu/vongdefu.github.io"
+      repoid="R_kgDON_DwvQ"
+      category="Announcements"
+      categoryid="DIC_kwDON_Dwvc4CnWcs"
+      mapping="pathname"
+      term="Welcome to giscus!"
+      reactionsenabled="1"
+      emitmetadata="0"
+      inputposition="top"
+      loading="lazy"
+      :theme="isDark ? 'dark' : 'light'"
+      :key="route.path"
+    ></Giscus>
+  </div>
+</template>
+
+<script setup>
+import Giscus from "@giscus/vue";
+import { useRoute, useData } from "vitepress";
+
+const route = useRoute();
+const { isDark } = useData();
+</script>
---
+++ docs/.vitepress/theme/components/wechat.vue
@@ -0,0 +1,9 @@
+<!-- <template>
+  <div style="margin-top: 24px">
+    <img
+      src="https://vongdefu.github.io/02-java/juc/ch01-background/image/1741338781290.png"
+      alt="wechat"
+      style="width: 100%; max-width: 300px; margin: 0 auto; display: block"
+    />
+  </div>
+</template> -->
